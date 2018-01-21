b76ffe5c548a893d5ba28df75da623aa096583a7
ann.marinas@hotmail.com <ann.marinas@hotmail.com>
2018-01-22 01:42:26 +1300

    add srcServer

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+var express = require('express'); // minimal web framework for node.js
+var path = require('path'); // node.js registry path
+var open = require('open'); // used to open our site in the browser
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.listen(port, function(err){
+  if(err) {
+    console.log(err);
+  }
+  else {
+    open('http://localhost:' + port);
+  }
+});
